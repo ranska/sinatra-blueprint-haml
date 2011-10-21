d339190139eeecb8407ca49e4daecbae8a011556
Pierre-Alexandre <ranska@gmail.com>
2011-10-21 09:40:03 +0200

    extract text into partial

--- hi.rb
+++ hi.rb
@@ -6,7 +6,7 @@ require 'haml'
 
 helpers do
   def partial(page, options={})
-    haml page, options.merge!(:layout => false)
+    haml "partials/#{page}".to_sym, options.merge!(:layout => false)
   end
 end
 
